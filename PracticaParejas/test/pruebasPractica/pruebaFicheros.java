063031e43f022647c66c0fdc3bf05366ff1ddabf
Danny <dcalzadas@gmail.com>
2019-09-25 10:37:28 +0200

    añadido fichero

--- PracticaParejas/test/pruebasPractica/pruebaFicheros.java
+++ PracticaParejas/test/pruebasPractica/pruebaFicheros.java
@@ -6,30 +6,31 @@
 package pruebasPractica;
 
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import practicaparejas.HiloCrearFichero;
-import practicaparejas.QuitaEs1;
-import practicaparejas.QuitaEs2;
-import practicaparejas.QuitaEs3;
+
+import practicaparejas.*;
 
 /**
  *
  * @author Formacion
  */
 public class pruebaFicheros {
-    
-    public pruebaFicheros() {
-    }
-    
-    @Test
-    public void testLanzarFicheros(){
-        
-    }
+
+	public pruebaFicheros() {
+	}
+
+	@Test
+	public void testLanzarFicheros() {
+		
+
+	}
 
 	@Test
 	public void testSecuencial() {
@@ -38,21 +39,21 @@ public class pruebaFicheros {
 		QuitaEs1 metodo1 = new QuitaEs1();
 		QuitaEs2 metodo2 = new QuitaEs2();
 		QuitaEs3 metodo3 = new QuitaEs3();
-		HiloCrearFichero hFich1 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo1);
-		HiloCrearFichero hFich2 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo2);
-		HiloCrearFichero hFich3 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo3);
+		HiloCrearFichero hFich1 = new HiloCrearFichero("texto_esp.txt", metodo1);
+		HiloCrearFichero hFich2 = new HiloCrearFichero("texto_esp.txt", metodo2);
+		HiloCrearFichero hFich3 = new HiloCrearFichero("texto_esp.txt", metodo3);
 		hFich1.leerFicheroEjem();
 		hFich2.leerFicheroEjem();
 		hFich3.leerFicheroEjem();
-		System.out.println(tiempoTotal(inicio) + "ms Tardados");
+		System.out.println(tiempoTotal(inicio) + "ms Tardados Secuencial");
 
 	}
-	
-	public double tiempoInicio(){
+
+	public double tiempoInicio() {
 		return new Date().getTime();
 	}
-	
-	public double tiempoTotal(double inicio){
+
+	public double tiempoTotal(double inicio) {
 		return new Date().getTime() - inicio;
 	}
 }
