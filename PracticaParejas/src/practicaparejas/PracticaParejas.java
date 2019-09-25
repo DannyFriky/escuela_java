9b58375b533008692516665d7313f3afc38f477a
Danny <dcalzadas@gmail.com>
2019-09-25 09:32:19 +0200

    Añadido proyecto en parejas

---
+++ PracticaParejas/src/practicaparejas/PracticaParejas.java
@@ -0,0 +1,16 @@
+
+package practicaparejas;
+
+/**
+ * @author Danny
+ */
+public class PracticaParejas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
