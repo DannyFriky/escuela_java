cba45e0f054a7fabadb4b6142c93641ae902f43e
Sergio <sergio.fuenla92@gmail.com>
2019-09-25 09:55:03 +0200

    Añadido test

--- PracticaParejas/test/pruebasPractica/pruebaFicheros.java
+++ PracticaParejas/test/pruebasPractica/pruebaFicheros.java
@@ -21,25 +21,8 @@ public class pruebaFicheros {
     public pruebaFicheros() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+    @Test
+    public void testLanzarFicheros(){
+        
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
