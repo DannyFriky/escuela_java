42f2ea23e44ded6232b14ffc36e133713493c8dc
Danny <dcalzadas@gmail.com>
2019-09-25 09:54:37 +0200

    inicio secuencial

--- PracticaParejas/test/pruebasPractica/pruebaFicheros.java
+++ PracticaParejas/test/pruebasPractica/pruebaFicheros.java
@@ -11,35 +11,28 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import practicaparejas.HiloCrearFichero;
+import practicaparejas.QuitaEs1;
+import practicaparejas.QuitaEs2;
+import practicaparejas.QuitaEs3;
 
 /**
  *
  * @author Formacion
  */
 public class pruebaFicheros {
-    
-    public pruebaFicheros() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+	@Test
+	public void testSecuencial() {
+		QuitaEs1 metodo1 = new QuitaEs1();
+		QuitaEs2 metodo2 = new QuitaEs2();
+		QuitaEs3 metodo3 = new QuitaEs3();
+		HiloCrearFichero hFich1 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo1);
+		HiloCrearFichero hFich2 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo2);
+		HiloCrearFichero hFich3 = new HiloCrearFichero("C:\\Users\\alumno\\Desktop\\ESCUELA_java\\escuela_java.git\\01_EjemploJava\\texto_esp.txt", metodo3);
+		hFich1.leerFicheroEjem();
+		hFich2.leerFicheroEjem();
+		hFich3.leerFicheroEjem();
+
+	}
 }
